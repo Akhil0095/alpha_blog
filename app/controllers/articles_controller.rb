5ab47317a278eb13d4e1ffc63699b0d4e83760db
ROR Training <akhilsoni@gmail.com>
2022-06-28 13:16:45 +0530

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ end
 def create
 	
 	@article = Article.new(article_params)
-	@article.user = User.first
+	@article.user = current_user
 	if @article.save
 		flash[:success] = " The Article is successfully Created..!!"
 		redirect_to article_path(@article)
